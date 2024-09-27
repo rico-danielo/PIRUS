8e24bd8e8baa4bb0ecf03e97a7965946c526c193
AtomicFrogg <pico2758@usherbrooke.ca>
2024-09-27 14:23:45 -0400

    Ajout de la fonction de retour après la fin

--- main.cpp
+++ main.cpp
@@ -1,47 +1,63 @@
 #include <Arduino.h>
 #include <libRobux.h>
+
+//Définition des variables et des constantes
 #define GaucheVertPin = 1;
 #define GaucheRougePin = 2;
 #define AvantVertPin = 3;
 #define AvantRougePin = 4;
 
 
-// put function declarations here:
 bool GaucheVert;
 bool GaucheRouge;
 bool AvantVert;
 bool AvantRouge;
-bool DroiteVert;
-bool DroiteRouge;
+bool depart;
 int PosX = 1;
 int PosY = 0;
+float vitesse;
+char PosRetour[];
 
+//Fonction d'arrêt des moteurs
 void arret(){
   MOTOR_SetSpeed(RIGHT, 0);
   MOTOR_SetSpeed(LEFT, 0);
 };
 
+
+//Fonction d'avancer (plus longue distance que avanceX)
+//Ajoute aussi 1 à la position en Y à chaque utilisation
+//À utiliser seulement lors de déplacement dans l'axe des Y(5m)
 void avanceY(){
   MOTOR_SetSpeed(RIGHT,vitesse);
   MOTOR_SetSpeed(LEFT, vitesse);
-  PosY++;
 };
 
+//Fonction d'avancer (plus petite distance que avanceY)
+//Ne change pas la position en X dans cette fonction
 void avanceX(){
   MOTOR_SetSpeed(RIGHT,vitesse);
   MOTOR_SetSpeed(LEFT, vitesse);
 }
 
+
+//Fonction permettant une rotation de 90 degrés vers la droite
 void tourneDroit(){
   MOTOR_SetSpeed(RIGHT, -0.5*vitesse);
   MOTOR_SetSpeed(LEFT, 0.5*vitesse);
 };
 
+
+//Fonction permettant une rotation de 90 degrés vers la gauche
 void tourneGauche(){
   MOTOR_SetSpeed(RIGHT, 0.5*vitesse);
   MOTOR_SetSpeed(LEFT, -0.5*vitesse);
 };
 
+
+//Fonction permettant de lire les capteurs gauche
+//Retourne 1 si un obstacle est détecté
+//Retourne 0 si rien n'est détecté
 void CheckGauche(){
   GaucheRouge = digitalRead(GaucheRougePin);
   GaucheVert = digitalRead(GaucheVertPin); 
@@ -52,6 +68,9 @@ if (GaucheVert && GaucheRouge)
 return 1;
 }
 
+//Fonction permettant de lire les capteurs droite
+//Retourne 1 si un obstacle est détecté
+//Retourne 0 si rien n'est détecté
 void CheckAvant(){
   AvantRouge = digitalRead(AvantRougePin);
   AvantVert = digitalRead(AvantVertPin);
@@ -62,11 +81,16 @@ void CheckAvant(){
   return 1;
 }
 
+
+//Fonction décrivant le cas que le robot commence son positionnement à gauche 
+//Uniquement pour l'allée
+//Incrémente automatiquement la position de Y à la fin 
 void Gauche()
 {
 if (CheckAvant() = 0)
   {
     avanceY();
+      PosY++;
   }
 else
   {
@@ -77,6 +101,7 @@ else
     {
       tourneGauche();
       avanceY();
+      PosY++;
     }
     else
     {
@@ -84,15 +109,22 @@ else
       PosX++;
       tourneGauche();
       avanceY();
+      PosY++;
     }
   }
 }
 
+
+
+//Fonction décrivant le cas que le robot commence son positionnement au milieu
+//Uniquement pour l'allée
+//Incrémente automatiquement la position de Y à la fin
 void Milieu()
 {
   if (CheckAvant() = 0)
   {
     avanceY();
+    PosY++;
   }
   else
   {
@@ -103,20 +135,27 @@ void Milieu()
       PosX--;
       tourneDroit();
       avanceY();
+      PosY++;
     }
     tourneDroit();
     avanceX();
     PosX++;
     tourneGauche();
     avanceY();
+    PosY++;
   }
 }
 
 
+
+//Fonction décrivant le cas que le robot commence son positionnement à droite 
+//Uniquement pour l'allée
+//Incrémente automatiquement la position de Y à la fin
 void Droite(){
   if (CheckAvant() = 0)
   {
     avanceY();
+    PosY++;
   }
   else
   {
@@ -127,6 +166,7 @@ void Droite(){
     {
       tourneDroit();
       avanceY();
+      PosY++;
     }
     else
     {
@@ -134,46 +174,104 @@ void Droite(){
       PosX--;
       tourneDroit();
       avanceY();
+      PosY++;
     }
+  }
+}
+
+
+void retour()
+{
+  tourneDroit();
+  tourneDroit();
+  avanceY();
+  PosY--;
+  while(PosY > 0)
+  {
+    if (PosRetour[PosY-1]<PosX)
+   {
+      tourneDroit();
+      avanceX();
+      if (PosRetour[PosY-1]<PosX)
+      {
+       avanceX()
+     }
+     tourneGauche();
+   }
+   if(PosRetour[PosY-1]>PosX)
+   {
+     tourneGauche();
+     avanceX();
+     if (PosRetour[PosY-1]>PosX)
+     {
+       avanceX();
+     }
+     tourneDroit();
+   }
+   avanceY();
+   PosY--;
   }
 }
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+//initialisation des Pins
 void setup() {
-  // put your setup code here, to run once:
   pinMode(AvantVertPin, INPUT);
   pinMode(AvantRougePin, INPUT);
   pinMode(GaucheVertPin, INPUT);
   pinMode(GaucheRougePin, INPUT);
-  pinMode(DroiteVertPin, INPUT);
-  pinMode(DroiteRougePin, INPUT);
- PosX = 1;
- PosY = 0;
 }
 
 
 void loop(){
-while (PosY<5)
+
+//Boucle déclarant le départ à partir du sifflet
+if (/* condition */)
+{
+  depart = true;
+}
+
+
+//Boucle déclarant chaque cas possible selon la position du robot pour l'allée
+while (PosY<5 && depart)
 {
  switch (PosX)
 
 {
 case 0:
-  Gauche();
+    PosRetour[PosY]=0;
+    Gauche();
   break;
+
 case 1:
-  Milieu();
+     PosRetour[PosY]=1;
+     Milieu();
   break;
+
   case 2:
-  Droite();
+    PosRetour[PosY]=2;
+    Droite();
   break;
 
 default:
   break;
 }
 
+//Le robot arrête après avoir traversé la ligne de fin
 }
 arret();
+
+retour();
 }
