9c25edca261a34964bd6a570f33d33ef0911899e
Atomic-spec <bombardierjoey634@gmail.com>
2024-10-21 22:16:41 -0400

    Début de logique d'initialisation

--- Initialisation Octogone/src/main.cpp
+++ Initialisation Octogone/src/main.cpp
@@ -2,6 +2,59 @@
 
 // put function declarations here:
 int myFunction(int, int);
+int couleur == (0);
+int detecteur_de_ligne == (0);
+int Initilisation (couleur){
+  if (couleur == "rouge"){
+    while (detecteur_de_ligne == (0)){
+    avance(vitesse);
+    }
+  }
+  if (couleur == "jaune"){
+    while(detecteur_de_ligne == (1)){
+      avance (vitesse);
+    }
+    while (couleur != "noir"){
+      detecteur_de_ligne = (1);
+      avance(vitesse);
+    }
+      tourneGauche ();
+    while (couleur != "rouge"){
+      detecteur_de_ligne == (1);
+      avance(vitesse);
+    }
+    tourneGauche;
+    tourneGauche;
+  }
+  if (couleur == "vert"){
+    while(detecteur_de_ligne == (1)){
+      avance (vitesse);
+    }
+    while (couleur != "noir"){
+      detecteur_de_ligne = (1);
+      avance(vitesse);
+    }
+      tourneDroit ();
+    while (couleur != "rouge"){
+      detecteur_de_ligne == (1);
+      avance(vitesse);
+    }
+    tourneGauche;
+    tourneGauche;
+  }
+  if (couleur == "bleu"){
+    while (detecteur_de_ligne == (0)){
+      avance(vitesse);
+    }
+    while (couleur != "rouge"){
+      detecteur_de_ligne == (1);
+      avance(vitesse)
+    }
+    tourneGauche;
+    tourneGauche;
+  }
+
+}
 
 void setup() {
   // put your setup code here, to run once:
